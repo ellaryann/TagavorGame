003ad15f3d64285cdb645b0496b3a453ffc5208b
ellaryann <albert.ellaryan@tumo.org>
2023-07-19 17:59:40 +0400

    nkarneri popoxutyun

--- nkar.js
+++ nkar.js
@@ -2,21 +2,74 @@ const nkarner = [
     {
         "nkar1": "images/Tigran_2.jpg",
         "name1": "Տիգրան 2_րդ Մեծ",
-
+    },{
         "nkar2": "images/Pap.jpg",
-        "name2": "Աշոտ 2_րդ Երկաթ",
-
+        "name2": "Պապ",
+    },{
         "nkar3": "images/Gagik_1.jpg",
-        "name3": "Աշոտ 2_րդ Երկաթ",
-
+        "name3": "Գագիկ Առաջին",
+    }
         "nkar4": "images/Ashot_2.jpg",
-        "name4": "Աշոտ 2_րդ Երկաթ",
-
+        "name4": "Աշոտ 2_րդ",
+     {,{
         "nkar5": "images/Artavazd_2.jpg",
-        "name5": "Աշոտ 2_րդ Երկաթ",
-
+        "name5": "Արտավազդ 2_րդ",
+     },{
         "nkar6": "images/Arshak_2.jpeg",
-        "name6": "Աշոտ 2_րդ Երկաթ",
+        "name6": "Արշակ 2_րդ",
+
+        "nkar7": "images/George_1.jpeg",
+        "name7": "Գեորգի Աոաջին",
+
+        "nkar8": "images/George_4.jpeg",
+        "name8": "Գեորգի 4_րդ",
+
+        "nkar9": "images/George_5.jpeg",
+        "name9": "Գեորգի 5_րդ",
+
+        "nkar10": "images/George_6.jpeg",
+        "name10": "Գեորգի 6_րդ",
+
+        "nkar11": "images/Nikolas_2.jpeg",
+        "name11": "Նիկոլաս 2_րդ",
+
+        "nkar12": "images/Petros_1.jpeg",
+        "name12": "Պետրոս Աոաջին",
+
+        "nkar13": "images/Petros_3.jpeg",
+        "name13": "Պետրոս 3_րդ",
+
+        "nkar14": "images/Charles_3.jpeg",
+        "name14": "Չառլեզ 3_րդ",
+
+        "nkar15": "images/Nikolas_2.jpeg",
+        "name15": "Նիկոլաս 2_րդ",
+
+        "nkar15": "images/Nikol_1.jpeg",
+        "name15": "Նիկոլ Աոաջին Մեծ",
+
+        "nkar16": "images/Serjik.jpeg",
+        "name16": "Սերժ Առաջին",
+//taguhiner
+        "nkar17": "images/Ekaterina_1.jpeg",
+        "name17": "Եկատերինա Առաջին",
+
+        "nkar18": "images/Ekaterina_2.jpeg",
+        "name18": "Եկատերինա 2_րդ",
+
+        "nkar19": "images/Elizaveta_1.jpeg",
+        "name19": "Ելիզավետա Առաջին",
+
+        "nkar20": "images/Elizaveta_2.jpeg",
+        "name20": "Ելիզավետա 2_րդ ",
+
+        "nkar21": "images/Epige.jpeg",
+        "name21": "Էպիգե",
+
+        "nkar22": "images/Kleopatra.jpeg",
+        "name22": "Կլեոպատրա",
+
+
     }
 
 ]
