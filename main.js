92a5800af8214db7babed904ae18a85ef9c4b3c6
yourusername <your@email.com>
2023-07-20 17:51:50 +0400

    nkarnery random poxvum en

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
-let second = 10
+let second = 100
 let correctAnswer = 0
 let incorrectAnswer = 0
 function getElement(id) {
     return document.getElementById(id);
 }
-
-function check() {
-    let input;
-    try {
-        input = document.querySelector('input[name="option"]:checked').value;
-    } catch {
-        return;
-    }
-    if (input === "Տիգրան 2_րդ Մեծ") {
-        correctAnswer++
-        getElement("score").innerHTML = correctAnswer
-    } else {
-        incorrectAnswer++
-    }
-    clearInterval(checkInterval);
+function generateRandomTagavor() {
+    return nkarner [Math.floor(Math.random(nkarner.lenght-1)*10)]
 }
+function main(){
+    tag = generateRandomTagavor();
+    getElement("tagavor").src = tag.nkar;
+
+}
+
 function timer() {
     setTimeout(finish, second * 1000)
     getElement("time").innerHTML = second;
     let countdown = setInterval(function () {
-        second--;
+             main ();
+        second--; 
         getElement("time").textContent = second;
         if (second <= 0) {
             clearInterval(countdown);
@@ -41,6 +35,21 @@ function timer() {
     }, 1000)
 
 }
+function check() {
+    let input;
+    try {
+        input = document.querySelector('input[name="option"]:checked').value;
+    } catch {
+        return;
+    }
+    if (input === "Տիգրան 2_րդ Մեծ") {
+        correctAnswer++
+        getElement("score").innerHTML = correctAnswer
+    } else {
+        incorrectAnswer++
+    }
+    clearInterval(checkInterval);
+}
 function finish() {
     clearInterval(checkInterval);
     let parcentage = (correctAnswer / (correctAnswer + incorrectAnswer)) * 100;
@@ -48,6 +57,7 @@ function finish() {
 }
 let checkInterval = setInterval(check, 50);
 timer();
+
 
 
 
--- nkar.js
+++ nkar.js
@@ -1,75 +1,120 @@
 const nkarner = [
+    //tagavorner
     {
-        "nkar1": "images/Tigran_2.jpg",
-        "name1": "Տիգրան 2_րդ Մեծ",
-    },{
-        "nkar2": "images/Pap.jpg",
-        "name2": "Պապ",
-    },{
-        "nkar3": "images/Gagik_1.jpg",
-        "name3": "Գագիկ Առաջին",
-    }
-        "nkar4": "images/Ashot_2.jpg",
-        "name4": "Աշոտ 2_րդ",
-     {,{
-        "nkar5": "images/Artavazd_2.jpg",
-        "name5": "Արտավազդ 2_րդ",
-     },{
-        "nkar6": "images/Arshak_2.jpeg",
-        "name6": "Արշակ 2_րդ",
-
-        "nkar7": "images/George_1.jpeg",
-        "name7": "Գեորգի Աոաջին",
-
-        "nkar8": "images/George_4.jpeg",
-        "name8": "Գեորգի 4_րդ",
-
-        "nkar9": "images/George_5.jpeg",
-        "name9": "Գեորգի 5_րդ",
-
-        "nkar10": "images/George_6.jpeg",
-        "name10": "Գեորգի 6_րդ",
-
-        "nkar11": "images/Nikolas_2.jpeg",
-        "name11": "Նիկոլաս 2_րդ",
-
-        "nkar12": "images/Petros_1.jpeg",
-        "name12": "Պետրոս Աոաջին",
-
-        "nkar13": "images/Petros_3.jpeg",
-        "name13": "Պետրոս 3_րդ",
-
-        "nkar14": "images/Charles_3.jpeg",
-        "name14": "Չառլեզ 3_րդ",
-
-        "nkar15": "images/Nikolas_2.jpeg",
-        "name15": "Նիկոլաս 2_րդ",
-
-        "nkar15": "images/Nikol_1.jpeg",
-        "name15": "Նիկոլ Աոաջին Մեծ",
-
-        "nkar16": "images/Serjik.jpeg",
-        "name16": "Սերժ Առաջին",
-//taguhiner
-        "nkar17": "images/Ekaterina_1.jpeg",
-        "name17": "Եկատերինա Առաջին",
-
-        "nkar18": "images/Ekaterina_2.jpeg",
-        "name18": "Եկատերինա 2_րդ",
-
-        "nkar19": "images/Elizaveta_1.jpeg",
-        "name19": "Ելիզավետա Առաջին",
-
-        "nkar20": "images/Elizaveta_2.jpeg",
-        "name20": "Ելիզավետա 2_րդ ",
-
-        "nkar21": "images/Epige.jpeg",
-        "name21": "Էպիգե",
-
-        "nkar22": "images/Kleopatra.jpeg",
-        "name22": "Կլեոպատրա",
-
-
-    }
+        "nkar": "./images/Tigran_2.jpg",
+        "name": "Տիգրան 2_րդ Մեծ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Pap.jpeg",
+        "name": "Պապ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Gagik_1.jpg",
+        "name": "Գագիկ Առաջին",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Ashot_2.jpg",
+        "name": "Աշոտ 2_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Artavazd_2.jpg",
+        "name": "Արտավազդ 2_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Arshak_2.jpeg",
+        "name": "Արշակ 2_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/George_1.jpeg",
+        "name": "Գեորգի Աոաջին",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/George_4.jpeg",
+        "name": "Գեորգի 4_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/George_5.jpeg",
+        "name": "Գեորգի 5_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/George_6.jpeg",
+        "name": "Գեորգի 6_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Nikolas_2.jpeg",
+        "name": "Նիկոլաս 2_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Petros_1.jpeg",
+        "name": "Պետրոս Աոաջին",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Petros_3.jpeg",
+        "name": "Պետրոս 3_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Charles_3.jpeg",
+        "name": "Չառլեզ 3_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Nikolas_2.jpeg",
+        "name": "Նիկոլաս 2_րդ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Nikol_1.jpeg",
+        "name": "Նիկոլ Աոաջին Մեծ",
+        "tipe":"tagavor",
+    },
+    {
+        "nkar": "./images/Serjik.jpeg",
+        "name": "Սերժ Առաջին",
+        "tipe":"tagavor",
+    },
+    //taguhiner
+    {
+        "nkar": "./images/Ekaterina_1.jpeg",
+        "name": "Եկատերինա Առաջին",
+        "tipe":"taguhi",
+    },
+    {
+        "nkar": "./images/Ekaterina_2.jpeg",
+        "name": "Եկատերինա 2_րդ",
+        "tipe":"taguhi",
+    },
+    {
+        "nkar": "./images/Elizaveta_1.jpeg",
+        "name": "Ելիզավետա Առաջին",
+        "tipe":"taguhi",
+    },
+    {
+        "nkar": "./images/Elizaveta_2.jpeg",
+        "name":"Ելիզավետա 2_րդ ",
+        "tipe":"taguhi",
+    },
+    {
+        "nkar": "./images/Epige.jpeg",
+        "name": "Էպիգե",
+        "tipe":"taguhi",
+    },
+    {
+        "nkar": "./images/Kleopatra.jpeg",
+        "name": "Կլեոպատրա",
+        "tipe":"taguhi",
+    },
 
 ]
